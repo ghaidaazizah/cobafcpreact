a23aaa475befbdcf77ecaa8f6dff61dbc734496b
sholikul <sholikulardian@gmail.com>
2024-10-30 12:06:58 +0700

    debugging code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,13 @@
 import { Box } from "@chakra-ui/react";
-import { studentId, studentName } from "../Task";
+// import { studentId, studentName } from "../Task";
 
 const Footer = () => {
-    return (
-        <>
-            <Box className="footer" bg="gray.800" color="white" p={4} textAlign="center">
-                <p className="studentName">{studentName}</p>
-                <p className="studentId">{studentId}</p>
-            </Box>
-        </>
-    );
+  return (
+    <Box as='footer' className='footer' bg='gray.800' color='white' p={4} textAlign='center'>
+      <p className='studentName'>Ghaida Syafa Azizah</p>
+      <p className='studentId'>FS11503666</p>
+    </Box>
+  );
 };
 
 export default Footer;
